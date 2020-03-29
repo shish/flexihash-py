048cee6d53a275ce79a5c40e4bc34091994d50a6
Shish <shish@shishnet.org>
2020-03-29 04:40:16 +0100

    test exact values for compatibility

--- tests/test_hasher.py
+++ tests/test_hasher.py
@@ -11,20 +11,12 @@ class HasherTest(unittest.TestCase):
 
     def test_Crc32Hash(self):
         hasher = Crc32Hasher()
-        result1 = hasher.hash("test")
-        result2 = hasher.hash("test")
-        result3 = hasher.hash("different")
-
         self.assertEqual(hasher.hash("test"), 3632233996)
         self.assertEqual(hasher.hash("test"), 3632233996)
         self.assertEqual(hasher.hash("different"), 1812431075)
 
     def test_Md5Hash(self):
         hasher = Md5Hasher()
-        result1 = hasher.hash("test")
-        result2 = hasher.hash("test")
-        result3 = hasher.hash("different")
-
         self.assertEqual(hasher.hash("test"), "098f6bcd4621d373cade4e832627b4f6")
         self.assertEqual(hasher.hash("test"), "098f6bcd4621d373cade4e832627b4f6")
         self.assertEqual(hasher.hash("different"), "29e4b66fa8076de4d7a26c727b8dbdfa")
